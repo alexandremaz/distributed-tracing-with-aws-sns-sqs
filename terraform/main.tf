46461b313cc8392d79e5e2a98847a7031d614ca5
alexandremaz <alexandre@mazioui.com>
2025-10-05 22:02:07 +0200

    use topology.json

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,81 +42,72 @@ provider "aws" {
   }
 }
 
-resource "aws_sns_topic" "first_topic" {
-  name = "first-topic"
+locals {
+  topology = jsondecode(file("topology.json"))
+  
+  all_names = distinct(concat(
+    [for pub in local.topology.publishers : pub.name],
+    flatten([for pub in local.topology.publishers : pub.subscribers])
+  ))
+  
+  subscriptions = flatten([
+    for pub in local.topology.publishers : [
+      for sub in pub.subscribers : {
+        publisher  = pub.name
+        subscriber = sub
+        key        = "${pub.name}-to-${sub}"
+      }
+    ]
+  ])
 }
 
-resource "aws_sns_topic" "second_topic" {
-  name = "second-topic"
+resource "aws_sns_topic" "topics" {
+  for_each = { for pub in local.topology.publishers : pub.name => pub }
+  name     = "${each.value.name}-topic"
 }
 
-resource "aws_sqs_queue" "first_queue" {
-  name = "first-queue"
+resource "aws_sqs_queue" "queues" {
+  for_each = toset(local.all_names)
+  name     = "${each.value}-queue"
 }
 
-resource "aws_sqs_queue" "second_queue" {
-  name = "second-queue"
-}
+resource "aws_sqs_queue_policy" "queue_policies" {
+  for_each = aws_sqs_queue.queues
 
-resource "aws_sqs_queue_policy" "first_queue_policy" {
-  queue_url = aws_sqs_queue.first_queue.id
+  queue_url = each.value.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
+      for sub in local.subscriptions : {
         Effect    = "Allow"
         Principal = "*"
         Action    = "SQS:SendMessage"
-        Resource  = aws_sqs_queue.first_queue.arn
+        Resource  = each.value.arn
         Condition = {
           ArnEquals = {
-            "aws:SourceArn" = aws_sns_topic.second_topic.arn
+            "aws:SourceArn" = aws_sns_topic.topics[sub.publisher].arn
           }
         }
-      }
+      } if sub.subscriber == each.key
     ]
   })
 }
 
-resource "aws_sqs_queue_policy" "second_queue_policy" {
-  queue_url = aws_sqs_queue.second_queue.id
+resource "aws_sns_topic_subscription" "subscriptions" {
+  for_each = { for sub in local.subscriptions : sub.key => sub }
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "SQS:SendMessage"
-        Resource  = aws_sqs_queue.second_queue.arn
-        Condition = {
-          ArnEquals = {
-            "aws:SourceArn" = aws_sns_topic.first_topic.arn
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_sns_topic_subscription" "first_to_second" {
-  topic_arn = aws_sns_topic.first_topic.arn
+  topic_arn = aws_sns_topic.topics[each.value.publisher].arn
   protocol  = "sqs"
-  endpoint  = aws_sqs_queue.second_queue.arn
-}
-
-resource "aws_sns_topic_subscription" "second_to_first" {
-  topic_arn = aws_sns_topic.second_topic.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.first_queue.arn
+  endpoint  = aws_sqs_queue.queues[each.value.subscriber].arn
 }
 
 output "provisioning_summary" {
   value = {
-    first_topic_arn  = aws_sns_topic.first_topic.arn
-    second_topic_arn = aws_sns_topic.second_topic.arn
-    first_queue_arn  = aws_sqs_queue.first_queue.arn
-    second_queue_arn = aws_sqs_queue.second_queue.arn
+    topics = { for k, v in aws_sns_topic.topics : k => v.arn }
+    queues = { for k, v in aws_sqs_queue.queues : k => v.arn }
+    subscriptions = {
+      for k, v in aws_sns_topic_subscription.subscriptions : k => v.arn
+    }
   }
 }
